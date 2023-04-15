030130643490407acc34856ee402aa6a661baffe
juhwan1014 <jmoon63@my.bcit.ca>
2023-04-14 18:00:46 -0700

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+import TodoList from './components/TodoList';
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <TodoList />
     </div>
   );
 }
 
 export default App;
+
+
+// import './App.css';
+// import {useState} from 'react'
+
+// function App() {
+
+//   const [input, setInput] = useState("");
+
+//   return (
+//     <div className="App">
+//        <input value={input} type="text"
+//        onChange={(event) => setInput(event.target.value)}
+//        />
+//        <button>add</button>
+//     </div>
+//   );
+// }
+
+// export default App;
