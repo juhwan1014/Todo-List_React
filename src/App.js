113a5a44109e0d0cd188e36c6c0c21d4d268a2a7
juhwan1014 <jmoon63@my.bcit.ca>
2023-04-14 20:58:08 -0700

    return back

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
+
 import './App.css';
-
-import TodoList from './components/TodoList';
-
+import {useState} from 'react'
 
 function App() {
 
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-     <TodoList />
+       <input value={input} type="text"
+       onChange={(event) => setInput(event.target.value)}
+       />
+       <button>add</button>
     </div>
   );
 }
 
 export default App;
-
-
-// import './App.css';
-// import {useState} from 'react'
-
-// function App() {
-
-//   const [input, setInput] = useState("");
-
-//   return (
-//     <div className="App">
-//        <input value={input} type="text"
-//        onChange={(event) => setInput(event.target.value)}
-//        />
-//        <button>add</button>
-//     </div>
-//   );
-// }
-
-// export default App;
