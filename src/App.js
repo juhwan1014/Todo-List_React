824d5e02c4da54d571ab22b0142bbb13492d141c
juhwan1014 <jmoon63@my.bcit.ca>
2023-04-24 10:14:48 -0700

    lecture

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 
 import './App.css';
 import {useState} from 'react'
+import TodoList from './components/TodoList';
 
 function App() {
 
-  const [input, setInput] = useState("");
 
   return (
     <div className="App">
-       <input value={input} type="text"
-       onChange={(event) => setInput(event.target.value)}
-       />
-       <button>add</button>
+        <TodoList />
     </div>
   );
 }
